d1291be2f218f72f6304c800b706aa929e64215e
Gareth Larkan <gareth@cis.za.net>
2024-09-26 21:06:40 +0200

    Added update merkle function

--- src/Swapper.sol
+++ src/Swapper.sol
@@ -4,10 +4,11 @@ pragma solidity >=0.8.25;
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "./Interfaces/IWETH.sol";
 
-contract Swapper {
+contract Swapper is Ownable2Step {
     using SafeERC20 for IERC20;
 
     bytes32 public merkleRoot;
@@ -16,11 +17,15 @@ contract Swapper {
     IWETH public immutable weth;
 
     error Zero_Address();
+    error Invalid_Proof();
+    error Incorrect_Value();
+
+    event UpdatedMerkleRoot(bytes32 merkleRoot);
 
     constructor(
         address _uniswapV3SwapRouter,
         address _wethAddress
-    ) {
+    ) Ownable(msg.sender) {
         isZeroAddress(_uniswapV3SwapRouter);
         isZeroAddress(_wethAddress);
         swapRouter = ISwapRouter(_uniswapV3SwapRouter);
@@ -38,10 +43,10 @@ contract Swapper {
         bytes32[] memory _merkleProof
     ) external payable returns (uint256 amountReceived) {
         bytes32 node = keccak256(abi.encodePacked(msg.sender));
-        require(MerkleProof.verify(_merkleProof, merkleRoot, node), "Swapper: Invalid Proof");
+        if(!MerkleProof.verify(_merkleProof, merkleRoot, node)) revert Invalid_Proof();
 
         if (_tokenIn == address(weth)) {
-            require(msg.value == _amountIn, "Swapper: Incorrect ETH Amount");
+            if(msg.value != _amountIn) revert Incorrect_Value();
         } else {
             IERC20(_tokenIn).safeTransferFrom(msg.sender, address(this), _amountIn);
             IERC20(_tokenIn).approve(address(swapRouter), _amountIn);
@@ -59,6 +64,12 @@ contract Swapper {
         });
 
         amountReceived = swapRouter.exactInputSingle{value: _tokenIn == address(weth) ? _amountIn : 0}(params);
+    }
+
+    function updateMerkleRoot(bytes32 _newMerkleRoot) external onlyOwner {
+        merkleRoot = _newMerkleRoot;
+
+        emit UpdatedMerkleRoot(_newMerkleRoot);
     }
 
     function isZeroAddress(address _address) internal pure {
