ccc6d9c80252ad5c2f2b68df03704712159f82c5
Gareth Larkan <gareth@cis.za.net>
2024-09-26 22:00:22 +0200

    Added basic test for construcotr in Swapper contract

---
+++ test/Swapper/Constructor.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.25;
+import "../TestSetup.sol";
+
+contract SwapperConstructorTests is TestSetup {
+
+    function setUp() public {
+        setUpTests();
+    }
+
+    function test_SwapperVariablesInitiatedCorrectly() public {
+        assertEq(address(swapper.rewardToken()), address(rewardToken));
+        assertEq(address(swapper.SWAP_ROUTER()), uniswapRouterV3);
+        assertEq(address(swapper.WETH()), wEthAddress);
+    }
+}
